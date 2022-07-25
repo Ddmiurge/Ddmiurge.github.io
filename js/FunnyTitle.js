1fbf27426e4ff1525d382bfcca20c401c01a3dc8
Ddmiurge <1141001473@qq.com>
2022-07-25 14:53:54 +0800

    Site updated: 2022-07-25 14:53:50

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ麻衣学姐yyds！';
+         document.title = '心向东方宣传站';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ我永远喜欢时崎狂三！' + OriginTitle;
+         document.title = '心向东方宣传站' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
