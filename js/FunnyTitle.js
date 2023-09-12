5dfa5697a9cc20763239aeccb830e2eef8742fe8
Ddmiurge <1141001473@qq.com>
2023-09-12 12:01:49 +0800

    Site updated: 2023-09-12 12:01:48

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,13 +3,13 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '心向东方宣传站';
+         $('[rel="icon"]').attr('href', "/img/favicon.png");
+         document.title = '这么下头，这就跑啦';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '心向东方宣传站' + OriginTitle;
+         $('[rel="icon"]').attr('href', "/img/favicon.png");
+         document.title = '呃呃呃呃' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
